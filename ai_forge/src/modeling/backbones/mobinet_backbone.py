2a5f39be11f5a5c63f9b4adb43927bd209af9d1c
khanhminhtk <khanhminh.gt.154203@gmail.com>
2026-04-27 18:23:20 +0700

    feat(backbone): add mobinetv3 small for backbone

---
+++ ai_forge/src/modeling/backbones/mobinet_backbone.py
@@ -0,0 +1,18 @@
+from torchvision.models.mobilenetv3 import mobilenet_v3_small, MobileNet_V3_Small_Weights
+
+from src.modeling.backbones.base_backbone import BaseBackBone
+
+class MobileNetBackBone(BaseBackBone):
+    def __init__(
+        self,
+        device: str,
+        in_feature: int | None,
+        out_feature: int
+    ):
+        model = mobilenet_v3_small(weights=MobileNet_V3_Small_Weights.IMAGENET1K_V1)
+        super().__init__(
+            device=device,
+            model=model,
+            in_feature=in_feature,
+            out_feature=out_feature
+        )
